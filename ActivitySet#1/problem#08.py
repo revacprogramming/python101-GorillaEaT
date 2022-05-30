a1e33f8374da44ca4b58f1eb9f3ff8d9d93c7a37
GorillaEaT <mayur1000.m@gmail.com>
2022-05-30 09:01:23 +0530

    .

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,10 +1,14 @@
-# Use the file name mbox-short.txt as the file name
+z = 0
+m = 0
+y = 0.0
 fname = input("Enter file name: ")
 fh = open(fname)
 for line in fh:
-    if line.startswith("X-DSPAM-Confidence:"):
-         
-        
-        continue
-    print(line)
-print("Done")
+    if line.startswith('X-DSPAM-Confidence:'):
+        x = line.split()
+        for i in x:
+            if i[0].isdigit():
+                y = float(i)
+                m += y
+                z += 1
+print(f'Average spam confidence: {m/z}')
