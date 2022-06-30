d616973294273dc3a8537de622f1b7b505bfbb14
GorillaEaT <mayur1000.m@gmail.com>
2022-06-30 22:45:59 +0530

    /

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,23 +1,15 @@
-# Dictionaries
 name = input("Enter file:")
 if len(name) < 1:
     name = "mbox-short.txt"
 handle = open(name)
-d={}
-for line in handle:
-    if line.startswith('From:'):continue
-    if not line.startswith('From'):continue
-    st=line.split()
-    word=st[1]
-    if word not in d:
-        d[word]=1
-    else:
-        d[word]+=1
-    
-l=0
-s=''
-for a,b in d.items():
-    if b>l:
-        l=b
-        s=a
-print(s,l)
+d = {}
+for i in handle:
+    if i.startswith("From "):
+        x = i.split()
+        if x[1] in d:
+            d[x[1]] += 1
+        else:
+            d[x[1]] = 1
+y = max(d, key=d.get)
+o = max(d.values())
+print(y, o)
