7f04a17a3c206bfb1e94f998d4d8dcc24c0023c2
GorillaEaT <mayur1000.m@gmail.com>
2022-07-06 15:25:07 +0530

    ..

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -3,9 +3,8 @@ hand=input('Enter the file name:')
 handle=open(hand)
 total=0
 for line in handle:
-     line=line.rstrip()
-     ss=re.findall('[0-9]+',line)
-     for i in ss:
+     s=re.findall('[0-9]+',line)
+     for i in s:
          total+=int(i)
     
 print(total)
