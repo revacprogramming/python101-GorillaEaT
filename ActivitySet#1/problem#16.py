9374a062c16e5debc335d9c98f0cc2ff663fbe17
GorillaEaT <mayur1000.m@gmail.com>
2022-07-13 12:18:54 +0530

    ..

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -1,12 +1,19 @@
-# Databases
-# https://www.py4e.com/lessons/database
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-import urllib.request, urllib.parse, urllib.error
 
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
-counts = dict()
-for line in fhand:
-  words = line.decode().split()
-  for word in words:
-    counts[word] = counts.get(word, 0) + 1
-print(counts)
+url = input('Enter URL:')
+count = int(input('Enter count:'))
+position = int(input('Enter position:'))-1
+html = urlopen(url).read()
+
+soup = BeautifulSoup(html,"html.parser")
+href = soup('a')
+#print href
+
+for i in range(count):
+    link = href[position].get('href', None)
+    print (href[position].contents[0])
+    html = urlopen(link).read()
+    soup = BeautifulSoup(html,"html.parser")
+    href = soup('a')
